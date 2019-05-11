b8c279b6c10e56a6728d1657f087dea8d54bc435
Will <doggy.huang@gmail.com>
2019-05-11 11:55:10 +0800

    示範 GoRedirect 共用檢視的做法

---
+++ MVC5Course/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Redir()
+        {
+            ViewBag.Msg = "OK!";
+            ViewBag.Url = "/";
+            return View("GoRedirect");
+        }
+    }
+}
