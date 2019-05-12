a9892d3a236fc30407bb0910193359d08317b418
Will <doggy.huang@gmail.com>
2019-05-12 17:32:23 +0800

    示範 ActionResult 的各種用法

---
+++ MVC5Course/Controllers/ARController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ARController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return PartialView();
+        }
+
+        public ActionResult ContentTest()
+        {
+            return Content("71932749127349217348");
+        }
+
+        [Route("robots.txt")]
+        public string Robots()
+        {
+            return @"User-agent: *
+Disallow: /Account/*.*
+Disallow: /search
+Disallow: /search.aspx
+Disallow: /error404.aspx
+Disallow: /archive
+Disallow: /archive.aspx
+
+sitemaps: https://blog.miniasp.com/sitemap.axd
+";
+        }
+
+        public ActionResult FileTest(bool dl = false)
+        {
+            var file = Server.MapPath("~/Content/nasa-53884-unsplash.jpg");
+
+            if (dl)
+            {
+                return File(file, "image/jpeg", "Download.jpg");
+            }
+            else
+            {
+                return File(file, "image/jpeg");
+            }
+        }
+    }
+}
