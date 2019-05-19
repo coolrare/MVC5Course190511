cefd457796f2ecb8d3f6c5bd1e7828b8fb58963d
Will <doggy.huang@gmail.com>
2019-05-19 10:55:13 +0800

    示範簡單模型繫結語法

--- MVC5Course/Controllers/MBController.cs
+++ MVC5Course/Controllers/MBController.cs
@@ -8,10 +8,10 @@ namespace MVC5Course.Controllers
 {
     public class MBController : BaseController
     {
-        public ActionResult Index()
+        public ActionResult Index(string key1, string key2)
         {
-            ViewData["key1"] = "1";
-            ViewBag.key2 = "2";
+            ViewData["key1"] = key1;
+            ViewBag.key2 = key2;
 
             ViewData.Model = "4";
 
