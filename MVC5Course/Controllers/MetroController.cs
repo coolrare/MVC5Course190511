d0a24717c764812819821a129b021cd8477065ae
Will <doggy.huang@gmail.com>
2019-05-26 11:17:36 +0800

    練習用 Bootstrap_Metro_Dashboard 網頁版型進行 MVC 套版

---
+++ MVC5Course/Controllers/MetroController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MetroController : Controller
+    {
+        // GET: Metro
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Messages()
+        {
+            return View();
+        }
+    }
+}
