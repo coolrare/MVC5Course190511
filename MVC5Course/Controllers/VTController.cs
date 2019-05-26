5221e3fe2a718384fe310d54bef308ac948a69a8
Will <doggy.huang@gmail.com>
2019-05-26 10:52:44 +0800

    示範 View 與 Layout  的執行順序

--- MVC5Course/Controllers/VTController.cs
+++ MVC5Course/Controllers/VTController.cs
@@ -13,5 +13,10 @@ namespace MVC5Course.Controllers
         {
             return View();
         }
+
+        public ActionResult View1()
+        {
+            return View();
+        }
     }
 }
