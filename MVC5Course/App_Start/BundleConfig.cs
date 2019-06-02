cbef5e1a2916d6a912afe73b9179b53b5d6ad574
Will <doggy.huang@gmail.com>
2019-06-02 10:24:24 +0800

    練習使用 BundleConfig 功能

--- MVC5Course/App_Start/BundleConfig.cs
+++ MVC5Course/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace MVC5Course
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,6 +26,62 @@ namespace MVC5Course
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/metrojs").Include(
+
+                    "~/js/jquery-1.9.1.min.js",
+                    "~/js/jquery-migrate-1.0.0.min.js",
+
+                    "~/js/jquery-ui-1.10.0.custom.min.js",
+
+                    "~/js/jquery.ui.touch-punch.js",
+
+                    "~/js/modernizr.js",
+
+                    "~/js/bootstrap.min.js",
+
+                    "~/js/jquery.cookie.js",
+
+                    "~/js/fullcalendar.min.js",
+
+                    "~/js/jquery.dataTables.min.js",
+                    "~/js/excanvas.js",
+                    "~/js/jquery.flot.js",
+                    "~/js/jquery.flot.pie.js",
+                    "~/js/jquery.flot.stack.js",
+                    "~/js/jquery.flot.resize.min.js",
+
+                    "~/js/jquery.chosen.min.js",
+
+                    "~/js/jquery.uniform.min.js",
+
+                    "~/js/jquery.cleditor.min.js",
+
+                    "~/js/jquery.noty.js",
+
+                    "~/js/jquery.elfinder.min.js",
+
+                    "~/js/jquery.raty.min.js",
+
+                    "~/js/jquery.iphone.toggle.js",
+
+                    "~/js/jquery.uploadify-3.1.min.js",
+
+                    "~/js/jquery.gritter.min.js",
+
+                    "~/js/jquery.imagesloaded.js",
+
+                    "~/js/jquery.masonry.min.js",
+
+                    "~/js/jquery.knob.modified.js",
+
+                    "~/js/jquery.sparkline.min.js",
+
+                    "~/js/counter.js",
+
+                    "~/js/retina.js",
+                    "~/js/custom.js"
+                ));
         }
     }
 }
